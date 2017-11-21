29c5f4e54f31c814a8370698475be318a23aa1f8
michalkoziarski <michalkoziarski@gmail.com>
2017-11-21 19:09:41 +0100

    removed unused optimization criteria

--- experiments/schedule_preliminary.py
+++ experiments/schedule_preliminary.py
@@ -10,22 +10,21 @@ import datasets
 for dataset in datasets.names('preliminary'):
     for fold in range(1, 11):
         for classifier in ['KNN', 'CART', 'SVM', 'NB']:
-            for criterion in ['balance', 'minimize', 'maximize']:
-                for n_steps in [1000, 2000, 4000, 8000, 16000]:
-                    for gamma in [0.001, 0.01, 0.1, 1.0, 10.0]:
-                        trial = {
-                            'Algorithm': 'RBO',
-                            'Parameters': {
-                                'gamma': gamma,
-                                'n_steps': n_steps,
-                                'step_size': 0.001,
-                                'stop_probability': 0.0,
-                                'criterion': criterion,
-                                'classifier': classifier
-                            },
-                            'Dataset': dataset,
-                            'Fold': fold,
-                            'Description': 'Preliminary'
-                        }
+            for n_steps in [1000, 2000, 4000, 8000, 16000]:
+                for gamma in [0.001, 0.01, 0.1, 1.0, 10.0]:
+                    trial = {
+                        'Algorithm': 'RBO',
+                        'Parameters': {
+                            'gamma': gamma,
+                            'n_steps': n_steps,
+                            'step_size': 0.001,
+                            'stop_probability': 0.0,
+                            'criterion': 'balance',
+                            'classifier': classifier
+                        },
+                        'Dataset': dataset,
+                        'Fold': fold,
+                        'Description': 'Preliminary'
+                    }
 
-                        databases.add_to_pending(trial)
+                    databases.add_to_pending(trial)
